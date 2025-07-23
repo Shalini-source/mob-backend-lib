7b5b41e62f736f43f71877c24c2914cb91466acc
Shalini-source <62209298+Shalini-source@users.noreply.github.com>
2025-07-23 17:57:03 +0530

    Update MobWrapper.java

--- src/main/java/com/shalini/moblib/MobWrapper.java
+++ src/main/java/com/shalini/moblib/MobWrapper.java
@@ -1,9 +1,10 @@
 package com.shalini.moblib;
 
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 
 public class MobWrapper {
     public static String cleanText(String input) {
-        return StringUtils.strip(input);
+        //return StringUtils.strip(input);
+        return "Success";
     }
 }
