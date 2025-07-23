b591a3fc358c2ac778d1e866213baf56fa7b4460
Shalini-source <62209298+Shalini-source@users.noreply.github.com>
2025-07-23 16:15:20 +0530

    Create MobWrapper.java

---
+++ src/main/java/com/shalini/moblib/MobWrapper.java
@@ -0,0 +1,9 @@
+package com.shalini.moblib;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MobWrapper {
+    public static String cleanText(String input) {
+        return StringUtils.strip(input);
+    }
+}
